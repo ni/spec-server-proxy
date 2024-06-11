9fd660243d4577adb1afd931f79872656726c998
Aravind K. Murali <aravind.murali@ni.com>
2024-06-11 11:58:27 +0530

    test: Add acceptance tests

---
+++ ni-spec-server-proxy/tests/acceptance/test_workflow.py
@@ -0,0 +1,165 @@
+"""Contains test case to validate valid workflow."""
+
+import io
+
+from werkzeug import Client
+from werkzeug.datastructures import FileStorage
+
+from ni_spec_server_proxy.constants import ScmResponseStateCodes
+from ni_spec_server_proxy.main import TAKE_COUNT
+from tests.constants import (
+    DATA,
+    DISCIPLINE,
+    INVALID_MEASUREMENT_FILE_NAME,
+    INVALID_MEASUREMENT_FILE_PATH,
+    INVALID_PRODUCT_GET_SPEC_URL,
+    INVALID_PRODUCT_NAME,
+    STATE,
+    STATUS_INTERNAL_SERVER_RESPONSE_CODE,
+    STATUS_NOT_FOUND_RESPONSE_CODE,
+    STATUS_SUCCESS_RESPONSE_CODE,
+    SUCCESS_STATE_IN_FILE_UPLOAD,
+    VALID_MEASUREMENT_FILE_NAME,
+    VALID_MEASUREMENT_FILE_PATH,
+    VALID_PRODUCT_GET_SPEC_URL,
+    VALID_PRODUCT_NAME,
+    VALID_PRODUCT_REVISION,
+)
+
+
+def test___connected_to_sle___product_available____returns_success_response(client: Client):
+    products_response = client.get("/niscm/public/products")
+
+    jsonified_products_response = products_response.get_json()
+
+    assert products_response.status_code == STATUS_SUCCESS_RESPONSE_CODE
+    assert len(jsonified_products_response[DATA]) <= TAKE_COUNT
+    assert jsonified_products_response[STATE] == ScmResponseStateCodes.SUCCESS
+
+    specs_response = client.get(VALID_PRODUCT_GET_SPEC_URL)
+
+    jsonified_specs_response = specs_response.get_json()
+
+    assert specs_response.status_code == STATUS_SUCCESS_RESPONSE_CODE
+    assert len(jsonified_specs_response[DATA]) != 0
+    assert jsonified_specs_response[STATE] == ScmResponseStateCodes.SUCCESS
+
+    with open(
+        VALID_MEASUREMENT_FILE_PATH,
+        "rb",
+    ) as f:
+        file_content = f.read()
+    files = {
+        "formCollection": (
+            FileStorage(
+                stream=io.BytesIO(file_content),
+                filename=VALID_MEASUREMENT_FILE_NAME,
+                content_type="text/csv",
+            )
+        )
+    }
+
+    file_upload_response = client.post(
+        f"/niscm/public/data/upload/{VALID_PRODUCT_NAME}/{VALID_PRODUCT_REVISION}/{DISCIPLINE}",
+        data=files,
+        content_type="multipart/form-data",
+    )
+
+    jsonified_file_upload_response = file_upload_response.get_json()
+
+    assert file_upload_response.status_code == STATUS_SUCCESS_RESPONSE_CODE
+    assert jsonified_file_upload_response[DATA]["fileName"] == VALID_MEASUREMENT_FILE_NAME
+    assert not jsonified_file_upload_response[DATA]["errors"]
+    assert jsonified_file_upload_response[STATE] == SUCCESS_STATE_IN_FILE_UPLOAD
+
+
+def test___connected_to_sle___product_unavailable___return_not_found_response(client: Client):
+    products_response = client.get("/niscm/public/products")
+
+    jsonified_products_response = products_response.get_json()
+
+    assert products_response.status_code == STATUS_SUCCESS_RESPONSE_CODE
+    assert len(jsonified_products_response[DATA]) <= TAKE_COUNT
+    assert jsonified_products_response[STATE] == ScmResponseStateCodes.SUCCESS
+
+    response = client.get(INVALID_PRODUCT_GET_SPEC_URL)
+
+    jsonified_response = response.get_json()
+
+    assert response.status_code == STATUS_NOT_FOUND_RESPONSE_CODE
+    assert not jsonified_response[DATA]
+    assert jsonified_response[STATE] == ScmResponseStateCodes.FAILURE
+
+    with open(
+        VALID_MEASUREMENT_FILE_PATH,
+        "rb",
+    ) as f:
+        file_content = f.read()
+    files = {
+        "formCollection": (
+            FileStorage(
+                stream=io.BytesIO(file_content),
+                filename=VALID_MEASUREMENT_FILE_NAME,
+                content_type="text/csv",
+            )
+        )
+    }
+
+    response = client.post(
+        f"/niscm/public/data/upload/{INVALID_PRODUCT_NAME}/{VALID_PRODUCT_REVISION}/{DISCIPLINE}",
+        data=files,
+        content_type="multipart/form-data",
+    )
+
+    jsonified_response = response.get_json()
+
+    assert response.status_code == STATUS_NOT_FOUND_RESPONSE_CODE
+    assert not jsonified_response[DATA]
+    assert jsonified_response[STATE] == ScmResponseStateCodes.FAILURE
+
+
+def test___connected_to_sle___invalid_measurement_file___return_server_error_response(
+    client: Client,
+):
+    products_response = client.get("/niscm/public/products")
+
+    jsonified_products_response = products_response.get_json()
+
+    assert products_response.status_code == STATUS_SUCCESS_RESPONSE_CODE
+    assert len(jsonified_products_response[DATA]) <= TAKE_COUNT
+    assert jsonified_products_response[STATE] == ScmResponseStateCodes.SUCCESS
+
+    specs_response = client.get(VALID_PRODUCT_GET_SPEC_URL)
+
+    jsonified_specs_response = specs_response.get_json()
+
+    assert specs_response.status_code == STATUS_SUCCESS_RESPONSE_CODE
+    assert len(jsonified_specs_response[DATA]) != 0
+    assert jsonified_specs_response[STATE] == ScmResponseStateCodes.SUCCESS
+
+    with open(
+        INVALID_MEASUREMENT_FILE_PATH,
+        "rb",
+    ) as f:
+        file_content = f.read()
+    files = {
+        "formCollection": (
+            FileStorage(
+                stream=io.BytesIO(file_content),
+                filename=INVALID_MEASUREMENT_FILE_NAME,
+                content_type="text/csv",
+            )
+        )
+    }
+
+    response = client.post(
+        f"/niscm/public/data/upload/{VALID_PRODUCT_NAME}/{VALID_PRODUCT_REVISION}/{DISCIPLINE}",
+        data=files,
+        content_type="multipart/form-data",
+    )
+
+    jsonified_response = response.get_json()
+
+    assert response.status_code == STATUS_INTERNAL_SERVER_RESPONSE_CODE
+    assert not jsonified_response[DATA]
+    assert jsonified_response[STATE] == ScmResponseStateCodes.FAILURE
